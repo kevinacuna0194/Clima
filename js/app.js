acc13ddba30bd47c73b7191178a70e36ef761015
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2022-11-29 19:30:22 -0300

    Validando el Formulario

--- js/app.js
+++ js/app.js
@@ -10,7 +10,38 @@ function buscarClima(e) {
     e.preventDefault();
 
     /** Validar */
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
 
+    if (ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
 
     /** Consultariamos la API */
+}
+
+function mostrarError(mensaje) {
+    // console.log(mensaje);
+
+    const alerta = document.querySelector('.bg-red-100');
+
+    if (!alerta) {
+        /** Crear una alerta*/
+        const alerta = document.createElement('div');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+        <strong class="font-extrabold">¡Error!</strong>
+        <span class="block font-semibold">${mensaje}</span>
+    `;
+
+        container.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
 }
